49f20d8b3e1c33772b3f1121594dc76003c17884
Everton windows desktop <ramiresero@gmail.com>
2021-06-30 19:51:17 -0300

    iniciei a implementação do router HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+
+)
+
+
 function App() {
   return (
     <div>
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
