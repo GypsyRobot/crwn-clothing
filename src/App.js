717ba5a648d4047abd3cd11fdffdf9b48d12c77e
Everton windows desktop <ramiresero@gmail.com>
2021-07-07 19:06:08 -0300

    primeiro esqueleto de redux montado para o user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -9,37 +10,30 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component.jsx';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.actions';
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+    const { setCurrentUser } = this.props; //to destructurando a propriedade setCurrentUser de dentro do this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
 
           // console.log(this.state);
         });
       }
       else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -51,7 +45,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -62,4 +56,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App); //sintaxe do connect é (mapSTATEtoprops,mapDISPATCHtoprops), como eu to mandando dispatch, tem que colocar NULL no local do state!
--- src/components/sign-in/sig-in.component.jsx
+++ src/components/sign-in/sig-in.component.jsx
@@ -63,7 +63,7 @@ class SignIn extends React.Component {
             required />
           <div className=' buttons'>
             <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+            <CustomButton type='button' onClick={SignInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
           </div>
         </form>
 
